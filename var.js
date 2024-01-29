262a7185e77f5de4960841af7d63710c96e5ae69
usama <im.uxama85@gmail.com>
2024-01-29 10:56:15 +0500

    function/array

--- var.js
+++ var.js
@@ -1,24 +1,69 @@
-f3 = (updateVlue1) => {
-    updateVlue1(6, 7);
-}
+// f3 = (updateVlue1) => {
+//     updateVlue1(6, 7);
+// }
 
 
-const f1 = ()  =>{
-  var a = 2;
-  var b = 3;
-
- 
-};
-
-const updateVlue = (NewA, NewB) =>{
-    a = NewA;
-    b = NewB;
-}
-
-f3(updateVlue);
-console.log(a, b)
-
-
+// const f1 = ()  =>{
+//   var a = 2;
+//   var b = 3;
 
  
-// f1(updateVlue)
+// };
+
+// const updateVlue = (NewA, NewB) =>{
+//     a = NewA;
+//     b = NewB;
+// }
+
+// f3(updateVlue);
+// console.log(a, b)
+
+
+
+
+
+
+
+
+
+
+
+// // +++++++++++++++++++++++++++++++++++++++++++LOOP+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// for ( let i = 0; i < 11; i++){
+//   if (i % 2 != 0){
+//     continue;
+//   }
+//   // console.log(i);
+// }
+
+
+// let i = 0;
+// while(i < 11){
+//   if (i % 2 != 0){
+//     continue
+//   }
+//   // console.log(i);
+// }
+
+// do{
+//   if (i % 2 != 0){
+//     continue
+//   }
+//   // console.log(i);
+// }while(i < 11)
+
+
+
+// // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+const arr = ["abc", 1 , "hell", 3]
+const arr1 = ["bcd", 3, "def", arr]
+
+for( let i = 0; i < arr1.length; i++){
+  console.log(arr1);
+}
+
+// // console.log()
